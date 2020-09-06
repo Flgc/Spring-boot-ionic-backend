cb15258e249fe0e55e46bed2e7ff6dbbf294e2c0
Fabio Luis Guia da Conceição <ml@fabiotecnico1.biz>
2020-09-06 19:07:15 -0300

    Implementando pimeira classe de dominio - Categoria

--- cursomc/src/main/java/biz/fabiotecnico1/cursomc/CursomcApplication.java
+++ cursomc/src/main/java/biz/fabiotecnico1/cursomc/CursomcApplication.java
@@ -1,4 +1,4 @@
-package biz.fabiotecnico1;
+package biz.fabiotecnico1.cursomc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- cursomc/src/main/java/biz/fabiotecnico1/cursomc/resources/CategoriaResource.java
+++ cursomc/src/main/java/biz/fabiotecnico1/cursomc/resources/CategoriaResource.java
@@ -1,15 +1,28 @@
 package biz.fabiotecnico1.cursomc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import biz.fabiotecnico1.cursomc .domain.Categoria;
 
 @RestController						
 @RequestMapping(value="/categorias")			//Name do end-point rest
 public class CategoriaResource {
 	
 	@RequestMapping(method=RequestMethod.GET)
-	public static String listar() {
-		return "REST está funcionando";
+	public List<Categoria> listar() {				//Método para retornar uma lista de categoria
+		
+		Categoria cat1 = new Categoria(1, "Informática");
+		Categoria cat2 = new Categoria(2, "Escritório");
+		
+		List<Categoria> lista = new ArrayList<>();
+		lista.add(cat1);
+		lista.add(cat2);
+		
+		return lista;
 	}
 }
