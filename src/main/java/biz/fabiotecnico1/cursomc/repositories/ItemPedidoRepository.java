c7c5912a02f567f7d7641153947b83a60e3acee2
Fabio Luis <ml@fabiotecnico1.biz>
2020-10-20 00:04:35 -0300

    Implementando ItemPedido e ItemPedidoPK

---
+++ src/main/java/biz/fabiotecnico1/cursomc/repositories/ItemPedidoRepository.java
@@ -0,0 +1,10 @@
+package biz.fabiotecnico1.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import biz.fabiotecnico1.cursomc.domain.ItemPedido;
+
+@Repository
+public interface ItemPedidoRepository extends JpaRepository<ItemPedido, Integer>{
+}
