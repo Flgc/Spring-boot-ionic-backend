2110bf0b6a7dba38e73f87539fd726e113ee55a4
Fabio Luis <ml@fabiotecnico1.biz>
2020-10-27 22:58:15 -0300

    Listando todas Categorias com DTO

--- src/main/java/biz/fabiotecnico1/cursomc/services/CategoriaService.java
+++ src/main/java/biz/fabiotecnico1/cursomc/services/CategoriaService.java
@@ -1,5 +1,6 @@
 package biz.fabiotecnico1.cursomc.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -45,4 +46,8 @@ public class CategoriaService {
 			throw new DataIntegrityException("Não é possível excluir uma categoria que possui produtos"); 
 		}
 	}
+	
+	public List<Categoria> findAll(){
+		return repo.findAll();
+	} 
 }
