9b80b612898fd5502d7b7f333e0e2f81137cf586
Fabio Luis <ml@fabiotecnico1.biz>
2020-10-29 11:21:49 -0300

    Validacao sintatica com Bean validation

--- src/main/java/biz/fabiotecnico1/cursomc/services/CategoriaService.java
+++ src/main/java/biz/fabiotecnico1/cursomc/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import biz.fabiotecnico1.cursomc.domain.Categoria;
+import biz.fabiotecnico1.cursomc.dto.CategoriaDTO;
 import biz.fabiotecnico1.cursomc.repositories.CategoriaRepository;
 import biz.fabiotecnico1.cursomc.services.exceptions.DataIntegrityException;
 import biz.fabiotecnico1.cursomc.services.exceptions.ObjectNotFoundException;
@@ -57,5 +58,9 @@ public class CategoriaService {
 	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		return repo.findAll(pageRequest);	
+	}
+	
+	public Categoria fromDTO(CategoriaDTO objDto) {
+		return new Categoria(objDto.getId(), objDto.getNome());
 	}
 }
