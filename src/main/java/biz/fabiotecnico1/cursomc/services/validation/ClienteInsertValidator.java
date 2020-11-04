5619cd3aed2320c2886e63b213aa4454a9b7f369
Fabio Luis <ml@fabiotecnico1.biz>
2020-11-04 17:47:58 -0300

    Validacao customizada: Bloqueio da inclusao do cliente com email existente

--- src/main/java/biz/fabiotecnico1/cursomc/services/validation/ClienteInsertValidator.java
+++ src/main/java/biz/fabiotecnico1/cursomc/services/validation/ClienteInsertValidator.java
@@ -6,12 +6,20 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import biz.fabiotecnico1.cursomc.domain.Cliente;
 import biz.fabiotecnico1.cursomc.domain.emums.TipoCliente;
 import biz.fabiotecnico1.cursomc.dto.ClienteNewDTO;
+import biz.fabiotecnico1.cursomc.repositories.ClienteRepository;
 import biz.fabiotecnico1.cursomc.resources.exception.FieldMessage;
 import biz.fabiotecnico1.cursomc.services.validation.utils.BR;
 
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO>{
+	
+	@Autowired
+	private ClienteRepository repo;
+	
 	@Override
 	public void initialize(ClienteInsert ann) {
 	}
@@ -30,6 +38,10 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
 			list.add(new FieldMessage("cpfOuCnpj", "CNPJ inválido"));
 		}
 		
+		Cliente aux = repo.findByEmail(objDto.getEmail());
+		if (aux != null) {
+			list.add(new FieldMessage("email", "Email já existente"));
+		}
 		
 		for (FieldMessage e : list) {
 			context.disableDefaultConstraintViolation();
