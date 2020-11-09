111ed0e3632295f1b22fd399f21cfb27aac6baf0
Fabio Luis <ml@fabiotecnico1.biz>
2020-11-08 21:47:53 -0300

    Criando profile de teste

---
+++ src/main/java/biz/fabiotecnico1/cursomc/services/PedidoService (1).java
@@ -0,0 +1,26 @@
+package biz.fabiotecnico1.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import biz.fabiotecnico1.cursomc.domain.Pedido;
+import biz.fabiotecnico1.cursomc.repositories.PedidoRepository;
+import biz.fabiotecnico1.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class PedidoService {
+	
+	@Autowired						
+	private PedidoRepository repo;
+	
+	public Pedido find(Integer id) {				
+		
+		Optional<Pedido> obj = repo.findById(id);	
+		
+		return obj.orElseThrow(()-> new ObjectNotFoundException( 
+				"Objeto não encontrado! Id: " + id + ", Tipo: "  
+				+ Pedido.class.getName()));
+	}	
+}
